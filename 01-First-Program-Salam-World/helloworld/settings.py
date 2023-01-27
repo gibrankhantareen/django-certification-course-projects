c2f1aaadf07b9ad339c1c37dad83b9d42f461366
Gibran Khan Tareen <gibrantareen@gmail.com>
2023-01-28 01:38:46 +0530

    Update settings.py

--- 01-First-Program-Salam-World/helloworld/settings.py
+++ 01-First-Program-Salam-World/helloworld/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0a&dpc#5rwdti8g#%7kg31axxi7x_wp$_oljcholl8-gj%srfq'
+SECRET_KEY = 'nahi dikhaunga'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
