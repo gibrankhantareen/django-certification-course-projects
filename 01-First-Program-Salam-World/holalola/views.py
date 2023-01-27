cdf68dea09925d188b8d7a0b4acbe5bd518c52b1
Gibran Khan Tareen <gibrantareen@gmail.com>
2023-01-28 01:22:13 +0530

    My First Salam World App

--- 01-First-Program-Salam-World/holalola/views.py
+++ 01-First-Program-Salam-World/holalola/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def NayaHomePageView(request):
-    return HttpResponse('Assalamualaikum means May Peace be upon you')
+    return HttpResponse('Assalamualaikum means May Peace be upon you. This is my first Django App. Made by Gibran Khan Tareen')
