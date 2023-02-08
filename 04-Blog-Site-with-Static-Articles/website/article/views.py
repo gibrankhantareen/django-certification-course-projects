ef0d8783d1912f44bb21b07afbcd42ba788b5bae
Gibran Khan Tareen <gibrantareen@gmail.com>
2023-02-08 18:49:15 +0530

    Creating a static blog which fetches the Data from Admin Panel

---
+++ 04-Blog-Site-with-Static-Articles/website/article/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView
+ 
+# Create your views here.
+from .models import Article
+
+class ArticleListView(ListView):
+    model = Article
+    template_name="home.html"
